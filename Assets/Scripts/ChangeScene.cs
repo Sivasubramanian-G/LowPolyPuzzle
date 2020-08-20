099d2a0e37257c293cea22940b0d250e5a6f177c
Sivasubramanian-G <sivasubramanian5.2000@gmail.com>
2020-08-20 18:00:59 +0530

    SceneChange successful Trying LWRP

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            SceneManager.LoadScene("SampleScene1");
+        }
+    }
+}
